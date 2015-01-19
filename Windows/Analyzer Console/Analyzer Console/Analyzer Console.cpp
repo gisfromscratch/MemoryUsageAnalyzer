cce689fdf8ab1c3e8db8fd74509282953d3e0e45
J.T <gisfromscratch@live.de>
2015-01-19 23:49:54 +0100

    #1: Implemented a kind of random "memory eater" as described in MSDB EatMem stress testing article

--- Windows/Analyzer Console/Analyzer Console/Analyzer Console.cpp
+++ Windows/Analyzer Console/Analyzer Console/Analyzer Console.cpp
@@ -3,11 +3,17 @@
 
 #include "stdafx.h"
 
+#include <chrono>
+#include <cstdlib>
+#include <iomanip>
 #include <iostream>
+#include <memory>
 #include <Psapi.h>
+#include <random>
 #include <VectorAllocator.h>
 
 using namespace edu::memory;
+using namespace std::chrono;
 using namespace std;
 
 void PrintMemoryInfo(DWORD processID)
@@ -76,19 +82,94 @@ static void PrintMemoryUsage()
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	// Print info
-	//PrintMemoryUsage();
-	PrintMemoryInfo(GetCurrentProcessId());
+	// Command line parsing
+	auto duration = 3;
+	auto cycleCount = 8;
+	const int conversionToBytes = 1024 * 1024;
+	switch (argc)
+	{
+		case 2:
+		{
+			// Assume unicode
+			wstring durationArgument(argv[1]);
+			if (0 == durationArgument.find_first_of(L"-d"))
+			{
+				duration = stoi(durationArgument.substr(2, durationArgument.size()));
+			}
+			break;
+		}
 
-	// Throw some allocation scenarios in
-	std::size_t numberOfElements = 1e6;
-	VectorAllocator vectorAllocator(numberOfElements);
-	wcout << vectorAllocator.scenarioName() << endl;
-	vectorAllocator.allocate();
+		default:
+			if (0 < argc)
+			{
+				wcout << endl << argv[0] << L" [-d]" << endl;
+				wcout << endl << L"  -d\tDefines the duration of the allocation scenarios." << endl;
+			}
+			break;
+	}
 
-	// Print info
-	//PrintMemoryUsage();
-	PrintMemoryInfo(GetCurrentProcessId());
+	wcout << endl << L"Allocating memory for " << duration << L" minutes." << endl << endl;
+
+	try
+	{
+		// Random percent
+		const double LowerDistributionBound = 0.3;
+		const double UpperDistributionBound = 0.7;
+		uniform_real_distribution<double> distribution(LowerDistributionBound, UpperDistributionBound);
+		random_device randomDevice;
+		std::mt19937 randomEngine(randomDevice());
+
+		// Print header
+		auto consoleOutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+		CONSOLE_SCREEN_BUFFER_INFO originalColors;
+		GetConsoleScreenBufferInfo(consoleOutHandle, &originalColors);
+		//SetConsoleTextAttribute(consoleOutHandle, WHITE | BGBLACK);
+		wcout << L"Elapsed Time\tAvailable Physical\tAvailable Virtual\tAllocated Memory" << endl;
+
+		// Start time
+		time_point<system_clock> startTime = system_clock::now();
+		time_point<system_clock> endTime = startTime + minutes(duration);
+		DWORD BreakDuration = 3000;
+		while (system_clock::now() < endTime)
+		{
+			for (auto cycle = 0; cycle < cycleCount; cycle++)
+			{
+				// Random percent calculating
+				double memoryPercent = distribution(randomEngine);
+
+				// Query the memory state
+				MEMORYSTATUS memoryStatus;
+				GlobalMemoryStatus(&memoryStatus);
+
+				// Allocate memory
+				auto bytesToAllocate = memoryPercent * memoryStatus.dwAvailVirtual;
+				auto memoryHandle = GlobalAlloc(GMEM_FIXED | GMEM_ZEROINIT, bytesToAllocate);
+				if (0 == memoryHandle)
+				{
+					SetConsoleTextAttribute(consoleOutHandle, RED | BGBLACK);
+					wcerr << L"Allocation failed. Trying again" << endl;
+					SetConsoleTextAttribute(consoleOutHandle, originalColors.wAttributes);
+					continue;
+				}
+
+				// Print the memory infos
+				std::chrono::duration<double> elapsedSeconds = system_clock::now() - startTime;
+				GlobalMemoryStatus(&memoryStatus);
+				wcout << elapsedSeconds.count() << L" [sec]\t";
+				wcout << memoryStatus.dwAvailPhys / conversionToBytes << L" [MB]\t";
+				wcout << memoryStatus.dwAvailVirtual / conversionToBytes << L" [MB]\t";
+				wcout << bytesToAllocate / conversionToBytes << L" [MB]" << endl;
+
+				// Free the allocated memory
+				GlobalFree(memoryHandle);
+			}
+			Sleep(BreakDuration);
+		}
+	}
+	catch (...)
+	{
+		wcerr << L"Running the allocation scenarios failed!" << endl;
+	}
 
 	system("PAUSE");
 	return 0;
--- Windows/Analyzer Console/Analyzer Console/stdafx.h
+++ Windows/Analyzer Console/Analyzer Console/stdafx.h
@@ -15,3 +15,15 @@
 
 
 // TODO: reference additional headers your program requires here
+const int BLACK = 0x0000;
+const int BLUE = 0x0001 | 0x0008;
+const int GREEN = 0x0002 | 0x0008;
+const int CYAN = 0x0003 | 0x0008;
+const int RED = 0x0004 | 0x0008;
+const int MAGENTA = 0x0005 | 0x0008;
+const int YELLOW = 0x0006 | 0x0008;
+const int WHITE = 0x0007 | 0x0008;
+
+const int BGWHITE = 0x0070 | 0x0080;
+const int BGBLACK = 0x0000 | 0x0000;
+const int BGGRAY = 0x0070;
